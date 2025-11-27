01baeffa5a5e7f90fe2874ed5c0e5dd53c0d2ccc
VG2310 <victor.guerra.z@usach.cl>
2025-11-27 03:25:21 -0300

    Add CORS config for production

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'https://vencepronto.cloud', // frontend oficial
+      'http://localhost:5173',     // por si desarrollas local
+    ],
+    credentials: true,
+    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
